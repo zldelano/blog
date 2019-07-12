f5f75b52d8d3ca7674a64282c9c0351a40ad0685
zldelano <del15002@byui.edu>
2019-07-11 19:47:22 -0600

    add some auth

--- server.js
+++ server.js
@@ -11,15 +11,22 @@ require('dotenv').config();
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString: connectionString });
 const PORT = process.env.PORT || 5000;
-const USER_ID = 'zdelano';
 const saltRounds = 8;
 var salt = bcrypt.genSaltSync(saltRounds);
 
 // build routes
-express()
+var app = express();
+app
     .use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
     .use(bodyParser.json())
     .use(bodyParser.urlencoded())
+    .use(session({
+        key: 'user_sid',
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        username: ''
+    }))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     // stuff we get from our API
@@ -41,13 +48,14 @@ express()
             } else if (result.rows.length > 1) {
                 res.send(500); // if we got more than 1 row, we should check our schema! 
             } else if (bcrypt.compareSync(req.body.password, result.rows[0].password)) {
+                req.session.username = req.body.username;
                 res.json({ message: "success" }); // compare plaintext POST password to hashed DB password
             } else {
                 res.json({ message: "failure" }); // failed to authenticate
             }
         });
     })
-    .get('/api/user', function(req, res) {
+    .get('/api/user', isAuthed, function(req, res) {
         const stmt = {
             name: 'fetch-user',
             text: 'SELECT * FROM blog_user'
@@ -60,7 +68,7 @@ express()
             res.json(result.rows);
         });
     })
-    .get('/api/blog_post', function(req, res) {
+    .get('/api/blog_post', isAuthed, function(req, res) {
         const stmt = {
             name: 'fetch-blogs',
             text: 'SELECT * FROM blog_post'
@@ -73,7 +81,7 @@ express()
             res.json(result.rows);
         });
     })
-    .get('/api/blog_post/:id', function(req, res) {
+    .get('/api/blog_post/:id', isAuthed, function(req, res) {
         const stmt = {
             name: 'fetch-blog-by-id',
             text: 'SELECT * FROM blog_post WHERE post_id=$1',
@@ -87,11 +95,11 @@ express()
             res.json(result.rows[0]);
         });
     })
-    .get('/api/blog_post/author/:author', function(req, res) {
+    .get('/api/blog_post/author/:author', isAuthed, function(req, res) {
         const stmt = {
             name: 'fetch-blogs-by-author',
             text: 'SELECT * FROM blog_post JOIN blog_user ON blog_user.username=$1',
-            values: [USER_ID]
+            values: [req.params.author]
         };
         // get blog posts by the author
         pool.query(stmt, function(err, result) {
@@ -101,11 +109,25 @@ express()
             res.json(result.rows);
         });
     })
-    .post('/api/blog_post', function(req, res) {
+    .get('/api/blog_post/profile', isAuthed, function(req, res) {
+        const stmt = {
+            name: 'fetch-blogs-for-profile',
+            text: 'SELECT * FROM blog_post JOIN blog_user ON blog_user.username=$1',
+            values: [req.session.username]
+        };
+        // get blog posts by the author
+        pool.query(stmt, function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.json(result.rows);
+        });
+    })
+    .post('/api/blog_post', isAuthed, function(req, res) {
         const stmt = {
             name: 'insert-new-blog-post',
             text: 'INSERT INTO blog_post (content, title, author) VALUES ($1,$2,$3)',
-            values: [req.body.content, req.body.title, USER_ID]
+            values: [req.body.content, req.body.title, req.session.username]
         };
         // make a new blog post
         pool.query(stmt, function(err, result) {
@@ -116,48 +138,76 @@ express()
             res.json(result.rows);
         });
     })
-    .get('/api/comments/blog_post/:blog_post_id', function(req, res) {
+    .get('/api/comments/blog_post/:blog_post_id', isAuthed, function(req, res) {
+        const stmt = {
+            name: 'get-blog-post-by-id',
+            text: 'SELECT * FROM blog_comment WHERE blog_post=$1',
+            values: [req.params.blog_post_id]
+        };
         // get blog posts by the author
-        pool.query('SELECT * FROM blog_comment WHERE blog_post=\'' + req.params.blog_post_id + '\'',
-            function(err, result) {
-                if (err) {
-                    return console.error('error running query', err);
-                }
-                res.json(result.rows);
-            });
+        pool.query(stmt, function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.json(result.rows);
+        });
     })
-    .post('/api/comments/blog_post/:blog_post_id', function(req, res) {
-        pool.query('INSERT INTO blog_comment (content, author, blog_post) VALUES (\'' +
-            req.body.content + '\', \'' + USER_ID + '\', \'' + req.body.postId + '\')',
-            function(err, result) {
-                if (err) {
-                    return console.error('error running query', err);
-                }
-                res.json(result.rows);
-            });
+    .post('/api/comments/blog_post/:blog_post_id', isAuthed, function(req, res) {
+        const stmt = {
+            name: 'insert-new-comment-on-blog',
+            text: 'INSERT INTO blog_comment (content, author, blog_post) VALUES ($1, $2, $3)',
+            values: [req.body.content, req.session.username, req.body.postId]
+        };
+        // post a comment to a specific blog
+        pool.query(stmt, function(err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.json(result.rows);
+        });
     })
     // client side stuff
-    .get('/blog_post/:id', function(req, res) {
+    // no auth middleware required
+    .get('/login', function(req, res) {
+        res.render('pages/login');
+    })
+    .get('/logout', function(req, res) {
+        req.session.destroy(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(session.username);
+                res.redirect('/login');
+            }
+        });
+    })
+    // the restof these routes require auth middleware
+    .get('/blog_post/:id', isAuthed, function(req, res) {
         // get a specific blog post by its ID
         res.render('pages/blog_post', {
             postId: req.params.id,
         });
     })
-    .get('/', function(req, res) {
+    .get('/', isAuthed, function(req, res) {
         res.render('pages/index');
     })
-    .get('/login', function(req, res) {
-        res.render('pages/login');
-    })
-    .get('/compose', function(req, res) {
+    .get('/compose', isAuthed, function(req, res) {
         res.render('pages/compose');
     })
-    .get('/profile', function(req, res) {
+    .get('/profile', isAuthed, function(req, res) {
         res.json({ "message": "there ain't nothin' here yet fool" });
     })
-    .get('/profile/posts', function(req, res) {
+    .get('/profile/posts', isAuthed, function(req, res) {
         res.render('pages/profile_posts', {
-            username: USER_ID
+            username: req.session.username
         });
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// middleware
+function isAuthed(req, res, next) {
+    if (req.session && req.session.username != undefined) {
+        return next();
+    }
+    res.redirect('/login');
+}
